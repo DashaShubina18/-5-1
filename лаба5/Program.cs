fb49cb8182808ec8e7ce162cfd66382ab464197e
Даша Шубіна <shubinad268@gmail.com>
2024-10-29 15:45:37 +0200

    Add project files.

---
+++ лаба5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[8, 8];
+            Random rand1 = new Random();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rand1.Next(0, 100);
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("-------------------------------");
+            int temp=0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = i; j < matrix.GetLength(1); j++)
+                {
+                    temp = matrix[i, j];
+                    matrix[i, j] = matrix[j, i];
+                    matrix[j, i] = temp;
+                }
+            }
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+         
+            
+
+        
+            
+        }
+    }
+}
